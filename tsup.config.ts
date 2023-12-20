31a91a663abb56531257a0222b4fd8ecd7f1a327
Christos Syrmakezis <sirmchristos@gmail.com>
2023-12-20 15:18:11 +0200

    Publish try

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'tsup';
 
 const CI = Boolean(process.env.CI);
+const SYMLINK_WATCH_MODE = Boolean(process.env.SYMLINK_WATCH_MODE);
 
 export default defineConfig({
   entry: ['src/index.ts'],
@@ -12,4 +13,5 @@ export default defineConfig({
   splitting: CI,
   minify: CI,
   dts: CI,
+  watch: !CI && SYMLINK_WATCH_MODE
  });
